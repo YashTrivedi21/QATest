059655885d4c59ea64607e13985e6b4ae040b299
Yash Trivedi <ybt2027@gmail.com>
2023-01-30 19:44:40 +0530

    Code cleanup

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -10,96 +10,167 @@ import java.time.Duration;
 import java.util.HashSet;
 import java.util.List;
 
+// Since there is only one page to be tested (user module)
+// all testcases are thus contained in main method itself
 public class HelloWorld {
 
+    // prints the list of elements names from the given list of web elements
     public static void printList(List<WebElement> divs){
-        HashSet<String> pills = new HashSet<>();
+        // set to store unique names
+        HashSet<String> elementText = new HashSet<>();
         for(var i : divs){
             try{
-                pills.add(i.getText());
-            } catch (org.openqa.selenium.NoSuchElementException ignored){
+                // adding only the text of current elements
+                elementText.add(i.getText());
             }
+            // ignore elements in between having no text value
+            catch (org.openqa.selenium.NoSuchElementException ignored){}
         }
-        for(var i : pills) System.out.println(i.replaceAll("\\d",""));
+        // removing any extra digit associated with the string and printing it
+        for(var i : elementText) System.out.println(i.replaceAll("\\d",""));
         System.out.println("\n\n\n");
     }
     public static void main(String[] args) throws InterruptedException {
+        // setting up the driver
         System.setProperty("webdriver.edge.driver", "F:\\Downloads\\edgedriver_win64\\msedgedriver.exe\\");
         WebDriver driver = new EdgeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 
-        // login to the neverland website
-        driver.get("https://neverland.freshprints.com/dashboard/login");
+        // go to the neverland website
+        String neverland = "https://neverland.freshprints.com/dashboard/login";
+        driver.get(neverland);
+
+        // maximize the window
+        // for mobile testing specific size value can be added
         driver.manage().window().maximize();
-        driver.findElement(By.id("form-email")).sendKeys("interns2022@freshprints.com");
-        driver.findElement(By.id("form-password")).sendKeys("interns2022");
+
+        // fill the details in the form
+        WebElement emailField = driver.findElement(By.id("form-email"));
+        String siteEmail = "interns2022@freshprints.com";
+        emailField.sendKeys(siteEmail);
+        WebElement passwordField = driver.findElement(By.id("form-password"));
+        String sitePassword = "interns2022";
+        passwordField.sendKeys(sitePassword);
+
+        // press the submit button
         driver.findElement(By.xpath("/html/body/div[4]/div/div/form/div/div[5]/div/button")).click();
 
-        // clicking on users button
+        // clicking on users button, avoiding the common error of element reference exception
+        WebElement userButton = driver.findElement(By.xpath("/html/body/div[2]/div/div[1]/div/div/div/ul/li[3]/a"));
         try {
-            driver.findElement(By.xpath("/html/body/div[2]/div/div[1]/div/div/div/ul/li[3]/a")).click();
+            userButton.click();
         } catch (org.openqa.selenium.StaleElementReferenceException ex){
-            driver.findElement(By.xpath("/html/body/div[2]/div/div[1]/div/div/div/ul/li[3]/a")).click();
+            userButton.click();
         }
 
         // sorting name column by clicking on it
+        // (similarly can be done for school, org and company field)
+        WebElement nameColumn = driver.findElement(By.xpath("//*[@id=\"ngx-datatable-basic\"]/div/datatable-header/div/div[2]/datatable-header-cell[1]"));
         try{
-            driver.findElement(By.xpath("//*[@id=\"ngx-datatable-basic\"]/div/datatable-header/div/div[2]/datatable-header-cell[1]")).click();
+            nameColumn.click();
         } catch (org.openqa.selenium.ElementClickInterceptedException e){
-            driver.findElement(By.xpath("//*[@id=\"ngx-datatable-basic\"]/div/datatable-header/div/div[2]/datatable-header-cell[1]")).click();
+            nameColumn.click();
         }
 
         // printing all 8 pills from the top
+        // pills include - all active, admin, client, printer etc
         List<WebElement> pills = driver.findElements(By.cssSelector("#tabs-wrapper div"));
         printList(pills);
 
         // pressing the create user button
+        // using explicit wait due to visibility issues
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         WebElement createUserButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"sticky-search-filter\"]/div[2]/app-button/button")));
         createUserButton.click();
 
         // print all 6 types of users that can be crated
-        List<WebElement> create_users = driver.findElements(By.cssSelector("body > app-custom-overlay > div > div > div.user-creation-wizard.ng-tns-c140-3.ng-star-inserted > app-wizard-create-select > div > div"));
-        printList(create_users);
+        // users include - admin, client, fulfilment center, Campus Manager etc
+        List<WebElement> createUsers = driver.findElements(By.cssSelector("body > app-custom-overlay > div > div > div.user-creation-wizard.ng-tns-c140-3.ng-star-inserted > app-wizard-create-select > div > div"));
+        printList(createUsers);
+
+        // pressing the cross button to close the same window
         driver.findElement(By.xpath("/html/body/app-custom-overlay/div/div/div[1]")).click();
 
         // pressing order button
-        driver.findElement(By.xpath("//*[@id=\"navbarSupportedContent\"]/ul/div/a[2]/div")).click();
+        WebElement orderButton = driver.findElement(By.xpath("//*[@id=\"navbarSupportedContent\"]/ul/div/a[2]/div"));
+        orderButton.click();
 
         // print all three types of orders
+        // orders include - regular order, group order and sample order
         List<WebElement> orders = driver.findElements(By.cssSelector("body > app-custom-overlay > div > div > app-create-order > div > div.create-order__orders.d-flex > a"));
         printList(orders);
-        WebElement element = driver.findElement(By.xpath("/html/body/app-custom-overlay/div/div/div"));
-        JavascriptExecutor executor = (JavascriptExecutor)driver;
-        executor.executeScript("arguments[0].click();", element);
 
-        // input functionality
-        driver.findElement(By.xpath("//*[@id=\"sticky-search-filter\"]/app-search-filter/div[1]/div/div/label/input")).sendKeys("rac client");
-        Thread.sleep(5000);
+        // closing the orders window using js executor
+        WebElement closeButton = driver.findElement(By.xpath("/html/body/app-custom-overlay/div/div/div"));
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        executor.executeScript("arguments[0].click();", closeButton);
+
+        // checking the input functionality
+        WebElement inputBar = driver.findElement(By.xpath("//*[@id=\"sticky-search-filter\"]/app-search-filter/div[1]/div/div/label/input"));
+        // string should have both cases and should not have perfect match but prefix match
+        String inputText = "rAc cliEnt";
+        inputBar.sendKeys(inputText);
+        // using sleep for better visualisation
+        Thread.sleep(2000);
 
         // pressing the create user button
         createUserButton.click();
+
+        // pressing the admin button
+        WebElement adminButton = driver.findElement(By.xpath("/html/body/app-custom-overlay/div/div/div[2]/app-wizard-create-select/div/div[1]"));
+        adminButton.click();
+
         // getting the warnings for the input fields
-        driver.findElement(By.xpath("/html/body/app-custom-overlay/div/div/div[2]/app-wizard-create-select/div/div[1]")).click();
-        driver.findElement(By.xpath("/html/body/app-custom-overlay/div/div/div[2]/div[1]/app-admin/div/div/aw-wizard/div/aw-wizard-step[1]/div/div[3]/app-contact-info/div/div[1]/div[2]/app-input/div/input")).sendKeys(" ");
-        driver.findElement(By.xpath("/html/body/app-custom-overlay/div/div/div[2]/div[1]/app-admin/div/div/aw-wizard/div/aw-wizard-step[1]/div/div[3]/app-contact-info/div/div[2]/div[2]/app-input/div/input")).sendKeys("");
-        driver.findElement(By.xpath("/html/body/app-custom-overlay/div/div/div[2]/div[1]/app-admin/div/div/aw-wizard/div/aw-wizard-step[1]/div/div[3]/app-contact-info/div/div[3]/div[2]/app-input/div/input")).sendKeys("");
+        WebElement fullName = driver.findElement(By.xpath("/html/body/app-custom-overlay/div/div/div[2]/div[1]/app-admin/div/div/aw-wizard/div/aw-wizard-step[1]/div/div[3]/app-contact-info/div/div[1]/div[2]/app-input/div/input"));
+        String fullNameString = " ";
+        WebElement phone = driver.findElement(By.xpath("/html/body/app-custom-overlay/div/div/div[2]/div[1]/app-admin/div/div/aw-wizard/div/aw-wizard-step[1]/div/div[3]/app-contact-info/div/div[2]/div[2]/app-input/div/input"));
+        String phoneNumberString = "";
+        WebElement email = driver.findElement(By.xpath("/html/body/app-custom-overlay/div/div/div[2]/div[1]/app-admin/div/div/aw-wizard/div/aw-wizard-step[1]/div/div[3]/app-contact-info/div/div[3]/div[2]/app-input/div/input"));
+        String emailString = "";
+
+        fullName.sendKeys(fullNameString);
+        phone.sendKeys(phoneNumberString);
+        email.sendKeys(emailString);
+
+        // using sleep for better visibility
         Thread.sleep(2000);
-        System.out.println(driver.findElement(By.xpath("/html/body/app-custom-overlay/div/div/div[2]/div[1]/app-admin/div/div/aw-wizard/div/aw-wizard-step[1]/div/div[3]/app-contact-info/div/div[1]/div[2]/app-input/div/div/div")).getText());
-        System.out.println(driver.findElement(By.xpath("/html/body/app-custom-overlay/div/div/div[2]/div[1]/app-admin/div/div/aw-wizard/div/aw-wizard-step[1]/div/div[3]/app-contact-info/div/div[2]/div[2]/app-input/div/div/div")).getText());
+
+        // printing the warnings
+        String nameWarning = driver.findElement(By.xpath("/html/body/app-custom-overlay/div/div/div[2]/div[1]/app-admin/div/div/aw-wizard/div/aw-wizard-step[1]/div/div[3]/app-contact-info/div/div[1]/div[2]/app-input/div/div/div")).getText();
+        String phoneWarning = driver.findElement(By.xpath("/html/body/app-custom-overlay/div/div/div[2]/div[1]/app-admin/div/div/aw-wizard/div/aw-wizard-step[1]/div/div[3]/app-contact-info/div/div[2]/div[2]/app-input/div/div/div")).getText();
+        System.out.println(nameWarning);
+        System.out.println(phoneWarning);
 
         // filling valid data to create user
-        driver.findElement(By.xpath("/html/body/app-custom-overlay/div/div/div[2]/div[1]/app-admin/div/div/aw-wizard/div/aw-wizard-step[1]/div/div[3]/app-contact-info/div/div[1]/div[2]/app-input/div/input")).sendKeys("donald trump");
-        driver.findElement(By.xpath("/html/body/app-custom-overlay/div/div/div[2]/div[1]/app-admin/div/div/aw-wizard/div/aw-wizard-step[1]/div/div[3]/app-contact-info/div/div[2]/div[2]/app-input/div/input")).sendKeys("9494994458");
-        driver.findElement(By.xpath("/html/body/app-custom-overlay/div/div/div[2]/div[1]/app-admin/div/div/aw-wizard/div/aw-wizard-step[1]/div/div[3]/app-contact-info/div/div[3]/div[2]/app-input/div/input")).sendKeys("dt@gmail.com");
-        driver.findElement(By.xpath("/html/body/app-custom-overlay/div/div/div[2]/div[1]/app-admin/div/div/aw-wizard/div/aw-wizard-step[1]/div/div[3]/app-button/button")).click();
+        fullNameString = "donald trump";
+        phoneNumberString = "9494994458";
+        emailString = "dt@gmail.com";
+        fullName.sendKeys(fullNameString);
+        phone.sendKeys(phoneNumberString);
+        email.sendKeys(emailString);
+
+        // pressing the set password button
+        WebElement setPassword = driver.findElement(By.xpath("/html/body/app-custom-overlay/div/div/div[2]/div[1]/app-admin/div/div/aw-wizard/div/aw-wizard-step[1]/div/div[3]/app-button/button"));
+        setPassword.click();
+
+        // using sleep for better clarity of visualisation
         Thread.sleep(2000);
-        driver.findElement(By.xpath("/html/body/app-custom-overlay/div/div/div[2]/div[1]/app-admin/div/div/aw-wizard/div/aw-wizard-step[2]/div/div[2]/div/div[3]/app-set-password/div/div[1]/div[2]/app-input/div/input")).sendKeys("1234");
-        driver.findElement(By.xpath("/html/body/app-custom-overlay/div/div/div[2]/div[1]/app-admin/div/div/aw-wizard/div/aw-wizard-step[2]/div/div[2]/div/div[3]/app-set-password/div/div[2]/div[2]/app-input/div/input")).sendKeys("1234");
-        driver.findElement(By.xpath("/html/body/app-custom-overlay/div/div/div[2]/div[1]/app-admin/div/div/aw-wizard/div/aw-wizard-step[2]/div/div[2]/div/div[3]/app-button/button")).click();
-        Thread.sleep(2000);
-        // going back to home page
-        driver.findElement(By.xpath("/html/body/app-custom-overlay/div/div/app-edit-user/div/div[3]/div/app-button/button")).click();
+
+        // setting the passwords
+        WebElement adminPassword = driver.findElement(By.xpath("/html/body/app-custom-overlay/div/div/div[2]/div[1]/app-admin/div/div/aw-wizard/div/aw-wizard-step[2]/div/div[2]/div/div[3]/app-set-password/div/div[1]/div[2]/app-input/div/input"));
+        WebElement adminPasswordConfirm = driver.findElement(By.xpath("/html/body/app-custom-overlay/div/div/div[2]/div[1]/app-admin/div/div/aw-wizard/div/aw-wizard-step[2]/div/div[2]/div/div[3]/app-set-password/div/div[2]/div[2]/app-input/div/input"));
+        String passwordString = "1234";
+        adminPassword.sendKeys(passwordString);
+        adminPasswordConfirm.sendKeys(passwordString);
+
+        // clicking the submit button
+        WebElement adminSubmit = driver.findElement(By.xpath("/html/body/app-custom-overlay/div/div/div[2]/div[1]/app-admin/div/div/aw-wizard/div/aw-wizard-step[2]/div/div[2]/div/div[3]/app-button/button"));
+        adminSubmit.click();
+
+        // going back to home page with back to work button
+        WebElement backToWorkButton = driver.findElement(By.xpath("/html/body/app-custom-overlay/div/div/app-edit-user/div/div[3]/div/app-button/button"));
+        backToWorkButton.click();
+
         Thread.sleep(2000);
         driver.quit();
     }
